768a3c8529dd2566fa0c6199f3f4aebadbb23c16
Marckie Zeender <mkzeender@gmail.com>
2024-12-27 19:55:52 -0800

    fix older version type checking

--- true_lazy_import/lazy_wrapper.py
+++ true_lazy_import/lazy_wrapper.py
@@ -17,8 +17,12 @@ _old_init_subclass = ForwardRef.__init_subclass__
 ForwardRef.__init_subclass__ = _noop
 
 
-class LazyImportedType(ForwardRef):
-    ...
+class LazyImportedType(ForwardRef): # type: ignore
+    def __init__(self, arg, *, module=None, owner=None, is_argument=True, is_class=False):
+        if sys.version_info >= (3, 14):
+            super().__init__(arg, module=module, owner=owner, is_argument=is_argument, is_class=is_class)
+        else:
+            super().__init__(arg, module=module, is_argument=is_argument, is_class=is_class)
 
 ForwardRef.__init_subclass__ = _old_init_subclass
 
